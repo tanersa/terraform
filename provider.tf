6388c0c5b5d6c5268177949ce6715ba9784ca423
tanersa <43732087+tanersa@users.noreply.github.com>
2022-01-22 16:06:39 -0500

    changed terraform path

--- provider.tf
+++ provider.tf
@@ -2,9 +2,10 @@ provider "aws" {
     region = var.aws_region
 }
 
+
 terraform {
   backend "s3" {
-      bucket = "losersameregionbucket"
+      bucket = "sharks-terra-backend-file"
       key = "terraform.tfstate"
       region = "us-east-2"
       dynamodb_table = "terraform"
