a9fab5d1dbd0ceb8d49f6752e78f25fd25fe8d4a
tanersa <43732087+tanersa@users.noreply.github.com>
2022-01-22 16:07:59 -0500

    changed terraform path

--- public-subnets.tf
+++ public-subnets.tf
@@ -6,7 +6,7 @@ locals {
 resource "aws_subnet" "sharks_public_subnet" {
   count                   = length(local.az_names)
   vpc_id                  = aws_vpc.sharks_vpc.id
-  cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone       = local.az_names[count.index]
   map_public_ip_on_launch = true
   tags = {
