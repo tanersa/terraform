c56f9b32f89b5f4ca06f9f1a968575dea44f75eb
tanersa <43732087+tanersa@users.noreply.github.com>
2022-03-06 00:49:59 -0500

    updated resource listener

--- asg.tf
+++ asg.tf
@@ -77,7 +77,6 @@ resource "aws_lb_listener" "web_listener" {
   port              = "80"
   protocol          = "HTTP"
 
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web_tg.arn
